e0d195bad21e8c3b3f670405d66ae281c49a3c93
present05 <91232599+present05@users.noreply.github.com>
2021-09-30 11:18:30 +0900

    VR UI sample
    - VR UI 샘플 제작용 리소스 추가
    - VR UI 샘플 Scene 제작

---
+++ Assets/Script/UIRunButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIRunButton : MonoBehaviour
+{
+    public Text contentText;
+    public void Run()
+    {
+        contentText.text = "æ»≥Á«œººø‰";
+    }
+
+}
